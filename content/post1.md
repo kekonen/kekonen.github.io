f72d3cf4c4b1d947d433fc22f8a2271a26073d97
Daniil Naumetc <daniil.naumetc@gmail.com>
2019-05-22 17:28:53 +0200

    updates

--- content/post1.md
+++ content/post1.md
@@ -1,7 +1,6 @@
-href: "post.html", title: "Kek", subtitle: "LOL", by: "me", date: 'May 22, 2019'
 Title: Hello
 Subtitle: Kek
-by: MEEe
-date: 15 March 1990
-
+By: MEEe
+Date: 15 March 1990
+-----
 Hello, this is my post
--- js/kekonen.js
+++ js/kekonen.js
@@ -1,7 +1,29 @@
-superagent.get('https://raw.githubusercontent.com/kekonen/kekonen.github.io/master/content/post1.md').end((err, res) => {
-  console.log(err)
+let posts = [
+  {href: "post.html", title: "Kek", subtitle: "LOL", by: "me", date: 'May 22, 2019'},
+  // {href: "post.html", title: "Kek1", subtitle: "LOL1", by: "me1", date: 'May 22, 2019'},
+  // {href: "post.html", title: "Kek2", subtitle: "LOL2", by: "me2", date: 'May 22, 2019'},
+]
 
-  console.log(res)
+superagent.get('https://raw.githubusercontent.com/kekonen/kekonen.github.io/master/content/post1.md').end((err, res) => {
+  const {text} = res;
+
+  const borderIndex = text.indexOf('-----')
+  const headers = text.substr(0, borderIndex)
+  const cont = text.substr(borderIndex + 6)
+  console.log('headers', text)
+
+  // console.log('cont', cont)
+
+
+  const title = headers.match(/Title: (.+)\\n/)
+  const subtitle = headers.match(/Subtitle: (.+)\\n/)
+  const by = headers.match(/By: (.+)\\n/)
+  const date = headers.match(/Date: (.+)\\n/)
+
+  // console.log(title
+  //   ,subtitle
+  //   ,by
+  //   ,date)
 
 })
 
@@ -20,12 +42,8 @@ var headerApp = new Vue({
 var postsApp = new Vue({
     el: '#postsApp',
     data: {
-      posts: [
-        {href: "post.html", title: "Kek", subtitle: "LOL", by: "me", date: 'May 22, 2019'},
-        // {href: "post.html", title: "Kek1", subtitle: "LOL1", by: "me1", date: 'May 22, 2019'},
-        // {href: "post.html", title: "Kek2", subtitle: "LOL2", by: "me2", date: 'May 22, 2019'},
-      ]
+      posts
     }
   })
 
-  console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
+  // console.log(moment().format('MMMM Do YYYY, h:mm:ss a'))
