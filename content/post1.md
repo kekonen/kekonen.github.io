d8e75752bda5f8ec9e2612b9baa5eca437ad2818
Daniil Naumetc <daniil.naumetc@gmail.com>
2019-05-22 17:00:46 +0200

    new page

---
+++ content/post1.md
@@ -0,0 +1,7 @@
+href: "post.html", title: "Kek", subtitle: "LOL", by: "me", date: 'May 22, 2019'
+Title: Hello
+Subtitle: Kek
+by: MEEe
+date: 15 March 1990
+
+Hello, this is my post
--- js/kekonen.js
+++ js/kekonen.js
@@ -1,3 +1,7 @@
+// const posts = superagent.get()
+
+
+
 var headerApp = new Vue({
     el: '#header',
     data: {
@@ -13,8 +17,8 @@ var postsApp = new Vue({
     data: {
       posts: [
         {href: "post.html", title: "Kek", subtitle: "LOL", by: "me", date: 'May 22, 2019'},
-        {href: "post.html", title: "Kek1", subtitle: "LOL1", by: "me1", date: 'May 22, 2019'},
-        {href: "post.html", title: "Kek2", subtitle: "LOL2", by: "me2", date: 'May 22, 2019'},
+        // {href: "post.html", title: "Kek1", subtitle: "LOL1", by: "me1", date: 'May 22, 2019'},
+        // {href: "post.html", title: "Kek2", subtitle: "LOL2", by: "me2", date: 'May 22, 2019'},
       ]
     }
   })
